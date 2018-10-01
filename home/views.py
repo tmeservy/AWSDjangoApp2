b700980c81cb3b98b6eb6f3699b7df5f7b89ef53
Ubuntu <ubuntu@ip-172-31-10-125.ec2.internal>
2018-10-01 23:11:30 +0000

    Changes to home view

--- home/views.py
+++ home/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world! You’re at the home index")
+    return HttpResponse("Hello, world! Behold the change!")
 
 
